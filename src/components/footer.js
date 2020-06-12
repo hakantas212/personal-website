5eb2b0e8eb0bd61c6d060ebb7756c480a19de452
hakantas212 <hakantas212@gmail.com>
2020-06-12 11:02:41 +0200

    init commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+       <footer>
+           <h1>Contact</h1>
+       </footer>
+    )
+}
+
+export default Footer
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function aboutPage() {
+    return (
+        <div>
+            <h1>About me</h1>
+            <p>sdfsdfsdfdgdfgdfgdsgdfgdfgdfgd</p>
+        </div>
+    )
+}
+
+export default aboutPage
