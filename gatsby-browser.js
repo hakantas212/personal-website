50ec75197e202f9cdf9c0089ea8927a13d2a4649
hakantas212 <hakantas212@gmail.com>
2020-07-13 21:08:57 +0200

    import typefaces

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './src/components/App';
-
+import "typeface-inter"
+import "typeface-archivo"
 export const wrapRootElement = ({ element }) => {
   return <App>{element}</App>;
 };
